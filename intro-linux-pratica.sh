ca7a04a3c405e3d6c6537469a2e350ae50c6e3f0
Jkasnese <guilhermelw.uefs@gmail.com>
2019-02-27 17:49:43 -0300

    Initial commit
    Script pra geral o jogo

---
+++ intro-linux-pratica.sh
@@ -0,0 +1,6 @@
+# Este script é um jogo para ensinar os comandos básicos de Linux
+# Desenvolvido 
+
+#!/bin/bash
+printf "Olá! Bem vindo à Oficina de Introdução ao Linux! \nA maioria dos nomes dos comandos no terminal tem relação com o verbo da ação que você deseja realizar. Em geral, usa-se as consoantes do verbo.\n"
+printf "Por exemplo, o comando para listar os conteúdos da pasta possui 2 letras. Tente acertar: \n"
